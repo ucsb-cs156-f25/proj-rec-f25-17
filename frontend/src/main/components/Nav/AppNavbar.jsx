b8cc4ceacd918c0be2612025a285b19e2f020103
Gupperfisher <charlie220@ucsb.edu>
2025-11-20 00:19:18 -0800

    Update AppNavbar.jsx

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -61,15 +61,7 @@ export default function AppNavbar({
                   </NavDropdown.Item>
                 </NavDropdown>
               )}
-              {currentUser && currentUser.loggedIn ? (
-                <>
-                  <Nav.Link as={Link} to="/requests/create">
-                    Recommendation Request
-                  </Nav.Link>
-                </>
-              ) : (
-                <></>
-              )}
+              
               {hasRole(currentUser, "ROLE_PROFESSOR") && (
                 <>
                   <Nav.Link as={Link} to="/requesttypes/all">
