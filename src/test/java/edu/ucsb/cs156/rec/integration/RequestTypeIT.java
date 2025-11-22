e1f9590100d170dda5a11e60d5e41af89f4a1009
Charlie Nava <charlie220@ucsb.edu>
2025-11-21 20:52:18 -0800

    made integration test to pass converage

---
+++ src/test/java/edu/ucsb/cs156/rec/integration/RequestTypeIT.java
@@ -0,0 +1,56 @@
+package edu.ucsb.cs156.rec.seed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.ucsb.cs156.rec.repositories.RequestTypeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RequestTypeSeederTests {
+
+  @Autowired private RequestTypeRepository requestTypeRepository;
+
+  @Autowired private RequestTypeSeeder requestTypeSeeder;
+
+  @Test
+  public void testSeederInsertsTypeOneRequestType() {
+    var result = requestTypeRepository.findByRequestType("CS Department BS/MS program");
+    assertTrue(result.isPresent(), "CS Department BS/MS program RequestType should be seeded");
+  }
+
+  @Test
+  public void testSeederInsertsTypeTwoRequestType() {
+    var result = requestTypeRepository.findByRequestType("Scholarship or Fellowship");
+    assertTrue(result.isPresent(), "Scholarship or Fellowship RequestType should be seeded");
+  }
+
+  @Test
+  public void testSeederInsertsTypeThreeRequestType() {
+    var result = requestTypeRepository.findByRequestType("MS program (other than CS Dept BS/MS)");
+    assertTrue(
+        result.isPresent(), "MS program (other than CS Dept BS/MS) RequestType should be seeded");
+  }
+
+  @Test
+  public void testSeederInsertsTypeFourRequestType() {
+    var result = requestTypeRepository.findByRequestType("PhD program");
+    assertTrue(result.isPresent(), "PhD program");
+  }
+
+  @Test
+  public void testSeederDoesNotInsertDuplicates() {
+
+    long initialCount = requestTypeRepository.count();
+
+    requestTypeSeeder.run();
+
+    long afterSecondRun = requestTypeRepository.count();
+
+    assertEquals(
+        initialCount,
+        afterSecondRun,
+        "Seeder should not insert duplicates when run multiple times");
+  }
+}
