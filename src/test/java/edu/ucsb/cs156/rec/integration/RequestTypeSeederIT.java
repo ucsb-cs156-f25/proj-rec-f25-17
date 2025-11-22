e5a3f5fe37e7c4c485872259c42c63b22246fa3b
Gupperfisher <charlie220@ucsb.edu>
2025-11-21 20:58:25 -0800

    renamed RequestTypeIT.java to RequestTypeSeederIT.java
    renamed RequestTypeIT.java to RequestTypeSeederIT.java, so to make file's name match with public function.

--- src/test/java/edu/ucsb/cs156/rec/integration/RequestTypeSeederIT.java
+++ src/test/java/edu/ucsb/cs156/rec/integration/RequestTypeSeederIT.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class RequestTypeSeederTests {
+public class RequestTypeSeederIT {
 
   @Autowired private RequestTypeRepository requestTypeRepository;
 
