5b96d7b9503e6dde3b46b2a1c79a9b942f5a3f03
Gupperfisher <charlie220@ucsb.edu>
2025-12-04 04:35:51 -0800

    Update AppNavbar.jsx

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -25,7 +25,7 @@ export default function AppNavbar({
       >
         <Container>
           <Navbar.Brand as={Link} to="/">
-            UCSB Rec
+            UCSB
           </Navbar.Brand>
 
           <Navbar.Toggle />
